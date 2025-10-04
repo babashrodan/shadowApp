6049be939ffb21e9120749a5fcfa539b087a68eb
ShadowBanker <134082467+babashrodan@users.noreply.github.com>
2025-10-04 19:17:28 -0400

    Update BioFueltransaction.js

--- models/BioFueltransaction.js
+++ models/BioFueltransaction.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const BioFuelschema = new mongoose.Schema({
+const BioFuelSchema = new mongoose.Schema({
   type: String,
   quantity: Number,
   date: { type: Date, default: Date.now },
   arms: { type: mongoose.Schema.Types.ObjectId, ref: 'Arms' }
 });
 
-module.exports = mongoose.model('ArmsTransaction', ArmsTransactionSchema);
+module.exports = mongoose.model('BioFuelTransaction', BioFuelSchema);
